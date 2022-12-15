6507925c63b2e81f30f3ddb02e172abda5f10610
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-12-15 12:32:31 +0530

    Client can subscribe to test channel through Pushpin

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-#Pushpin Playground
+# Pushpin Playground
+
+To start the origin server
+
+```
+docker-compose build
+docker-compose up
+```
+
+Pushpin routes file:
+
+```
+*,debug go-service:8080,over_http
+```
+
+Client request to subscribe to test channel:
+
+> websocat -v ws://localhost:7999/subscribe
+
+
+To publish event from terminal to test channel:
+
+```
+curl -d '{ "items": [ { "channel": "test", "formats": {
+    "ws-message": { "content": "hello there\n" } } } ] }' \
+    http://localhost:5561/publish/
+```
+
--- main.go
+++ main.go
@@ -3,32 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/fanout/go-gripcontrol"
-	"github.com/fanout/go-pubcontrol"
 	"github.com/gin-gonic/gin"
 	"io"
 	"io/ioutil"
-	"time"
-
 	"net/http"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.Handle("GET", "/data", func(context *gin.Context) {
-		fmt.Printf("REQ: %+v", context.Request)
-		fmt.Println("REQ.Header : ", context.Request.Header)
-		fmt.Println("Received request ...")
-
-		context.Writer.Header().Set("Content-Type", "text/plain")
-		context.Writer.Header().Set("Grip-Hold", "stream")
-		context.Writer.Header().Set("Grip-Channel", "test")
-		context.Status(http.StatusOK)
-		return
+	router.Handle("POST", "/echo", func(context *gin.Context) {
+		fmt.Println("Got /echo request on origin server")
+		Echo(context.Writer, context.Request)
 	})
-
-	router.Handle("POST", "/", func(context *gin.Context) {
-		HandleRequest(context.Writer, context.Request)
+	router.Handle("POST", "/subscribe", func(context *gin.Context) {
+		fmt.Println("Got /subscribe  request on origin server")
+		Subscribe(context.Writer, context.Request)
 	})
 
 	err := http.ListenAndServe(":8080", router)
@@ -37,61 +27,53 @@ func main() {
 	}
 }
 
-func HandleRequest(writer http.ResponseWriter, request *http.Request) {
-	// Validate the Grip-Sig header:
-	//if !gripcontrol.ValidateSig(request.Header["Grip-Sig"][0], "<key>") {
-	//	http.Error(writer, "GRIP authorization failed", http.StatusUnauthorized)
-	//	return
-	//}
+func Echo(writer http.ResponseWriter, request *http.Request) {
 
-	// Set the headers required by the GRIP proxy:
-	writer.Header().Set("Sec-WebSocket-Extensions", "grip; message-prefix=\"\"")
-	writer.Header().Set("Content-Type", "application/websocket-events")
-	// Decode the incoming WebSocket events:
 	body, _ := ioutil.ReadAll(request.Body)
-	fmt.Println("Body : ", string(body))
+	fmt.Println("/echo got ==> Body : ", string(body))
 
-	inEvents, err := gripcontrol.DecodeWebSocketEvents(string(body))
+	writer.Header().Set("Content-Type", "application/websocket-events")
+	writer.Write(body)
+}
+
+func Subscribe(writer http.ResponseWriter, request *http.Request) {
+
+	inputBody, _ := ioutil.ReadAll(request.Body)
+	fmt.Println("/echo got ==> Body : ", string(inputBody))
+
+	writer.Header().Set("Content-Type", "application/websocket-events")
+
+	inEvents, err := gripcontrol.DecodeWebSocketEvents(string(inputBody))
 	if err != nil {
-		panic("Failed to decode WebSocket events: " + err.Error())
+		panic("Failed to decode WebSocket inEvents: " + err.Error())
 	}
 
 	fmt.Printf("IN events.length: %d", len(inEvents))
 	fmt.Printf("IN events[0]: %+v", inEvents[0])
 
+	shouldSubscribe := false
+
 	if inEvents[0].Type == "OPEN" {
-		// Create the WebSocket control message:
+		fmt.Println("OPEN event came to origin server ! ")
+		writer.Header().Set("Sec-WebSocket-Extensions", `grip; message-prefix=""`)
+		shouldSubscribe = true
+	}
+
+	if shouldSubscribe {
 		wsControlMessage, err := gripcontrol.WebSocketControlMessage("subscribe",
 			map[string]interface{}{"channel": "test"})
 		if err != nil {
 			panic("Unable to create control message: " + err.Error())
 		}
 
-		// Open the WebSocket and subscribe it to a channel:
 		outEvents := []*gripcontrol.WebSocketEvent{
 			&gripcontrol.WebSocketEvent{Type: "OPEN"},
 			&gripcontrol.WebSocketEvent{Type: "TEXT",
 				Content: "c:" + wsControlMessage}}
+
 		_, err1 := io.WriteString(writer, gripcontrol.EncodeWebSocketEvents(outEvents))
 		if err1 != nil {
-			fmt.Println("Err writing: ", err1.Error())
+			fmt.Println("Err writing outEvents to writer: ", err1.Error())
 		}
-
-		fmt.Println("Abount to enter goroutine ...")
-		go func() {
-			// Wait 3 seconds and publish a message to the subscribed channel:
-			fmt.Println("Waiting for 10 seconds.......")
-			time.Sleep(10 * time.Second)
-
-			pub := gripcontrol.NewGripPubControl([]map[string]interface{}{
-				map[string]interface{}{"control_uri": "http://pushpin:5561"}})
-			format := &gripcontrol.WebSocketMessageFormat{
-				Content: []byte("Test WebSocket Publish!!")}
-			item := pubcontrol.NewItem([]pubcontrol.Formatter{format}, "", "")
-			err = pub.Publish("test", item)
-			if err != nil {
-				panic("Publish failed with: " + err.Error())
-			}
-		}()
 	}
 }
